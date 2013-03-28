79071d14f75277a7fc22a32b04398b84816c7769
Holly Fionte <hfionte@Hollys-MacBook-Pro.local>
2013-03-28 16:09:41 -0400

    Example website

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+$(document).ready(function () {
 
+	// Mobile menu
+	$('body').on('click', '#mobile-menu-trigger', function () {
+		$('#main-nav').toggleClass('open');
+	});
+
+	$('.slideshow').cycle().addClass('enabled');
+
+	// Functionality for destroying and enabling functionality based on browser width.
+	// Comment out slideshow initalization above before enabling this example.
+	/*
+
+	// Initialize slideshow, but not for mobile.
+	if ($(window).width() >= 570) {
+		$('.slideshow').cycle().addClass('enabled');
+	}
+	
+	// Display slideshow as individual images on mobile.
+	$(window).resize(function () {
+		var siteWidth = $(window).width();
+		if (siteWidth < 570 && $('.slideshow').hasClass('enabled')) {
+			$('.slideshow').cycle('destroy')
+				.removeAttr('style')
+				.removeClass('enabled')
+				.find('img').removeAttr('style');
+			console.log("destroy slideshow");
+		} else if (siteWidth >= 570 && ! $('.slideshow').hasClass('enabled')) {
+			$('.slideshow').cycle().addClass('enabled');
+			console.log("build slideshow");
+		}
+	});
+	
+	*/
+
+});
