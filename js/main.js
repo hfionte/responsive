91f47a9e33954b6a6399da0465e9d8b11bacfe55
Holly Fionte <hfionte@gmail.com>
2014-03-31 21:03:41 -0400

    updated to bowst template, added basics and testing pages

--- js/main.js
+++ js/main.js
@@ -8,28 +8,4 @@ $(document).ready(function () {
 	// Initialize slideshow
 	$('.slideshow').cycle().addClass('enabled');
 
-	// Functionality for destroying and enabling slideshow based on browser width.
-	// Comment out slideshow initalization above before enabling this example.
-	/*
-
-	// Initialize slideshow, but not for mobile.
-	if ($(window).width() >= 570) {
-		$('.slideshow').cycle().addClass('enabled');
-	}
-	
-	// Display slideshow as individual images on mobile.
-	$(window).resize(function () {
-		var siteWidth = $(window).width();
-		if (siteWidth < 570 && $('.slideshow').hasClass('enabled')) {
-			$('.slideshow').cycle('destroy')
-				.removeAttr('style')
-				.removeClass('enabled')
-				.find('img').removeAttr('style');
-		} else if (siteWidth >= 570 && ! $('.slideshow').hasClass('enabled')) {
-			$('.slideshow').cycle().addClass('enabled');
-		}
-	});
-	
-	*/
-
 });
